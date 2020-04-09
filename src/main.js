0832ef6eba3e48bd649ec1d89a99d2ccadbb2b1e
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-09 19:28:54 +0200

    reorganized repository and files to match Vue standards

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import router from './router'
 
-Vue.use(VueRouter)
 Vue.config.productionTip = false
-
-
-const routes = [
-
-]
-
-const router = new VueRouter({
-  routes
-})
 
 new Vue({
   router,
