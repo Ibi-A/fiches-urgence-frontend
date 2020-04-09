4b1da082def2d46b49f3a5ad895189922ac65341
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-09 19:19:38 +0200

    removed Vuetify + cleaned the project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import HelloWorld from './components/HelloWorld'
-import PersonDetails from './components/PersonDetails'
-import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter)
-
 Vue.config.productionTip = false
 
+
 const routes = [
-  { path: "/hello", component: HelloWorld },
-  { path: "/person-details/:personId", component: PersonDetails }
+
 ]
 
 const router = new VueRouter({
@@ -20,6 +16,5 @@ const router = new VueRouter({
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
