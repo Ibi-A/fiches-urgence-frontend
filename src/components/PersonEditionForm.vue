325d1c33853dc57b9665e36dadaa466f730255c8
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-10 11:38:50 +0200

    minor modifications to optimize buttons grouping

--- src/components/PersonEditionForm.vue
+++ src/components/PersonEditionForm.vue
@@ -14,8 +14,10 @@
       value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
       hint="Hint text"
     ></v-textarea>
-    <v-btn color='success' to='/persons/person-id'>Save</v-btn>
-    <v-btn color='warning' to='/persons/person-id'>Cancel</v-btn>
-    <v-btn color='error' to='/persons/person-id'>Delete</v-btn>
+    <v-item-group>
+      <v-btn color='success' to='/persons/person-id'>Save</v-btn>
+      <v-btn color='warning' to='/persons/person-id'>Cancel</v-btn>
+      <v-btn color='error' to='/persons/person-id'>Delete</v-btn>
+    </v-item-group>
   </v-form>
 </template>
--- src/views/Persons.vue
+++ src/views/Persons.vue
@@ -1,32 +1,40 @@
 <template>
-    <v-container class="pa-12">
-        <v-row>
-            <h1 class='display-3'>Persons view</h1>
-        </v-row>
-        
-        <v-row>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-            <person-info-card/>
-        </v-row>
-    </v-container>
+  <v-container class="pa-12">
+    <v-row>
+      <h1 class="display-3">Persons view</h1>
+    </v-row>
+
+    <v-row>
+      <v-item-group>
+        <v-btn color="primary">New</v-btn>
+      </v-item-group>
+    </v-row>
+
+    <v-row>
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+      <person-info-card />
+    </v-row>
+
+    
+  </v-container>
 </template>
 
 <script>
-import PersonInfoCard from '../components/PersonInfoCard.vue'
+import PersonInfoCard from "../components/PersonInfoCard.vue";
 
 export default {
-    name: 'persons',
-    components: {
-        PersonInfoCard
-    }
-}
+  name: "persons",
+  components: {
+    PersonInfoCard
+  }
+};
 </script>
