30cce821361bb09cc6e6063012776598007a16cd
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-12 02:48:55 +0200

    added icons to navigation bar and application toolbar

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,42 @@
     <v-navigation-drawer app>
       <v-list dense>
         <v-list-item link to="/">
+        <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Accueil</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item link to="/persons">
+          <v-list-item-icon><v-icon>mdi-account-group</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Personnes</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item link to="/residents">
+          <v-list-item-icon><v-icon>mdi-clipboard-account</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Résidents</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item link to="/contributors">
+          <v-list-item-icon><v-icon>mdi-account-tie</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Intervenants</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item link to="/health-mutuals">
+          <v-list-item-icon><v-icon>mdi-hospital</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Mutuelles</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item link to="/cities">
+          <v-list-item-icon><v-icon>mdi-home-city</v-icon></v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Villes</v-list-item-title>
           </v-list-item-content>
@@ -41,6 +47,7 @@
     </v-navigation-drawer>
 
     <v-app-bar app class="gradient" dark>
+      <v-icon class='mr-3'>mdi-note-multiple</v-icon>
       <v-toolbar-title>Mes fiches d'urgence</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
