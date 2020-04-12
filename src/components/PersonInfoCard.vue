d71944f4cb1949d56399bd1e574db69c4c85483a
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-12 03:15:21 +0200

    added icons to PersonInfoCard component

--- src/components/PersonInfoCard.vue
+++ src/components/PersonInfoCard.vue
@@ -1,9 +1,10 @@
 <template>
   <v-col xs="12" sm="12" md="6" lg="4" xl="3">
     <v-card hover @click="switchPage()">
-      <v-card-title class="teal--text">{{ person['firstName'] }} {{ person['lastName'] }}</v-card-title>
+      <v-card-title class="teal--text"><v-icon class="mr-1">mdi-account-box</v-icon>{{ person['firstName'] }} {{ person['lastName'].toUpperCase() }}</v-card-title>
       <v-card-subtitle>{{ person['address'] }}</v-card-subtitle>
       <v-card-text>
+        <v-icon class="mr-1">mdi-phone</v-icon>
         <strong>Numéro(s) de téléphone</strong>
         <ul>
           <li>(+33) 06.06.06.06.06</li>
