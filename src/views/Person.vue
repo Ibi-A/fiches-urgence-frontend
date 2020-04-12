6265a3ada1744b19b528215c7fcd8863fb295aeb
Ibi <ibi.andrianoasy+git@gmail.com>
2020-04-12 03:13:47 +0200

    changed page title

--- src/views/Person.vue
+++ src/views/Person.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="pa-12">
     <v-row>
-      <h1 class="display-3">Consultation/modification d'une personne</h1>
+      <h1 class="display-3">Modifier une personne</h1>
     </v-row>
     <v-row>
       <person-edition-form v-bind:personId="personId" />
